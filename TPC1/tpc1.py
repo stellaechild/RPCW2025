d00522f25c02a57b369a44c0f4fa8562c22f412e
Maria <miniimi2001@gmail.com>
2025-02-10 21:31:32 +0000

    ajustes

--- TPC1/tpc1.py
+++ TPC1/tpc1.py
@@ -29,6 +29,9 @@ ttl = """@prefix : <http://www.semanticweb.org/uminho/mirtilo/rpcw/2025/tpc1#> .
 ###  http://www.semanticweb.org/uminho/mirtilo/rpcw/2025/tpc1#Exame
 :Exame rdf:type owl:Class .
 
+###  http://www.semanticweb.org/uminho/mirtilo/rpcw/2025/tpc1#Clube
+:Clube rdf:type owl:Class .
+
 #################################################################
 #    Object Properties
 #################################################################
@@ -37,6 +40,11 @@ ttl = """@prefix : <http://www.semanticweb.org/uminho/mirtilo/rpcw/2025/tpc1#> .
 :temExame rdf:type owl:ObjectProperty ;
         rdfs:domain :Atleta ;
         rdfs:range :Exame .
+
+###  http://www.semanticweb.org/uminho/mirtilo/rpcw/2025/tpc1#pertenceAClube
+:pertenceAClube rdf:type owl:ObjectProperty ;
+        rdfs:domain :Atleta ;
+        rdfs:range :Clube .
 
 #################################################################
 #    Data Properties
@@ -50,22 +58,53 @@ ttl = """@prefix : <http://www.semanticweb.org/uminho/mirtilo/rpcw/2025/tpc1#> .
           rdfs:domain :Atleta ;
           rdfs:range xsd:int .
 
-:temData rdf:type owl:DatatypeProperty ;
-         rdfs:domain :Atleta ;
-         rdfs:range xsd:dateTime .
-
-:temApelido rdf:type owl:DatatypeProperty ;
-            rdfs:domain :Atleta ;
-            rdfs:range xsd:string .
+:temDataEMD rdf:type owl:DatatypeProperty ;
+         rdfs:domain :Exame ;
+         rdfs:range xsd:date .
 
 :temPrimeiroNome rdf:type owl:DatatypeProperty ;
                  rdfs:domain :Atleta ;
                  rdfs:range xsd:string .
 
+:temApelido rdf:type owl:DatatypeProperty ;
+            rdfs:domain :Atleta ;
+            rdfs:range xsd:string .
+
 :temNome rdf:type owl:DatatypeProperty ;
          rdfs:domain :Atleta ;
          rdfs:range xsd:string .
 
+:temIdade rdf:type owl:DatatypeProperty ;
+         rdfs:domain :Atleta ;
+         rdfs:range xsd:int .
+
+:temGenero rdf:type owl:DatatypeProperty ;
+         rdfs:domain :Atleta ;
+         rdfs:range xsd:string .
+
+:temMorada rdf:type owl:DatatypeProperty ;
+         rdfs:domain :Atleta ;
+         rdfs:range xsd:string .
+
+:temModalidade rdf:type owl:DatatypeProperty ;
+         rdfs:domain :Atleta ;
+         rdfs:range xsd:string .
+
+:temClube rdf:type owl:DatatypeProperty ;
+         rdfs:domain :Clube ;
+         rdfs:range xsd:string .
+
+:temEmail rdf:type owl:DatatypeProperty ;
+         rdfs:domain :Atleta ;
+         rdfs:range xsd:string .
+
+:eFederado rdf:type owl:DatatypeProperty ;
+         rdfs:domain :Atleta ;
+         rdfs:range xsd:boolean .
+
+:temResultado rdf:type owl:DatatypeProperty ;
+         rdfs:domain :Exame ;
+         rdfs:range xsd:boolean .
 
 #################################################################
 #    Individuals
@@ -76,27 +115,40 @@ ttl = """@prefix : <http://www.semanticweb.org/uminho/mirtilo/rpcw/2025/tpc1#> .
 for atleta in bd:
     atleta_id = f"a_{atleta['_id']}"
     exame_id = f"e_{atleta['_id']}"
+    clube_id = f"c_{atleta['clube'].replace(' ', '_')}"
 
     registo = f"""
 ###  {atleta_id}
 :{atleta_id} rdf:type owl:NamedIndividual ,
              :Atleta ;
-             :temId "{atleta['_id']}";
-             :temIndex "{atleta['index']}";
-             :temData "{atleta['dataEMD']}";
-             :temApelido "{atleta['nome']['último']}" ;
+             :temId "{atleta['_id']}" ;
+             :temIndex {atleta['index']} ;
              :temPrimeiroNome "{atleta['nome']['primeiro']}" ;
-             :temNome "{atleta['nome']['primeiro']} {atleta['nome']['último']}";
-             :temExame :{exame_id} .
+             :temApelido "{atleta['nome']['último']}" ;
+             :temNome "{atleta['nome']['primeiro']} {atleta['nome']['último']}" ;
+             :temIdade {atleta['idade']} ;
+             :temGenero "{atleta['género']}" ;
+             :temMorada "{atleta['morada']}" ;
+             :temModalidade "{atleta['modalidade']}" ;
+             :temEmail "{atleta['email']}" ;
+             :eFederado "{str(atleta['federado']).lower()}" ;
+             :temExame :{exame_id} ;
+             :pertenceAClube :{clube_id} .
 
 ###  {exame_id}
 :{exame_id} rdf:type owl:NamedIndividual ,
             :Exame ;
-            :temData "{atleta['dataEMD']}" .
+            :temDataEMD "{atleta['dataEMD']}" ;
+            :temResultado "{str(atleta['resultado']).lower()}" .
+
+###  {clube_id}
+:{clube_id} rdf:type owl:NamedIndividual ,
+            :Clube ;
+            :temClube "{atleta['clube']}" .
 """
     ttl += registo
 
-# Guardar o conteúdo TTL num ficheiro
+# Gerar o ficheiro TTL
 with open("atletas.ttl", "w", encoding="utf-8") as output_file:
     output_file.write(ttl)
 
